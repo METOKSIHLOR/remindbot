ce5878d4ab59acfffd8fb0bcecf53487d9c345dd
METOKS <pressfschool@gmail.com>
2025-10-05 14:05:53 +0200

    ....

--- aiodialog/group_settings/groups.py
+++ aiodialog/group_settings/groups.py
@@ -3,55 +3,15 @@ from aiogram_dialog import Dialog, Window, DialogManager
 from aiogram_dialog.widgets.kbd import Button, Select, Row, ScrollingGroup
 from aiogram_dialog.widgets.text import Const, Format
 from functools import partial
-from db.requests import get_user_groups, get_subgroups
+
+from aiodialog.dialogs.group_settings.functions import on_group_selected, change_page, groups_getter
+from aiodialog.dialogs.subgroup_settings.functions import subgroups_getter
+from aiodialog.dialogs.subgroup_settings.subgroups import subgroups_group
 from aiodialog.StatsGroup import GroupsSg, StartSg
 from aiodialog.dialogs.create_group.creategroup import back_button
+
 async def main_menu(callback: CallbackQuery, button: Button, manager: DialogManager):
     await manager.start(StartSg.main_menu)
-
-async def groups_getter(dialog_manager: DialogManager, **kwargs):
-    user = dialog_manager.event.from_user.id
-    page = dialog_manager.dialog_data.get("page", 0)
-
-    groups = await get_user_groups(user)
-    total = len(groups)
-
-    start = page * 8
-    end = start + 8
-    page_groups = groups[start:end]
-
-    return {
-        "groups": page_groups,
-        "page": page,
-        "total": total,
-    }
-
-async def on_group_selected(c, w, manager: DialogManager, item_id):
-    group_id = int(item_id)
-    await manager.start(state=GroupsSg.my_subgroups, data={"group_id": group_id})
-
-async def on_subgroup_selected(c, w, manager: DialogManager, item_id):
-    print(f"Выбрана подгруппа {item_id}")
-
-async def subgroups_getter(dialog_manager: DialogManager, **kwargs):
-    group_id = dialog_manager.start_data.get("group_id")
-    subgroups = await get_subgroups(group_id)
-    return {"subgroups": subgroups}
-
-subgroups_select = Select(
-    Format("{item.name}"),
-    id="subgroups_select",
-    item_id_getter=lambda x: x.sg_id,
-    items="subgroups",
-    on_click=on_subgroup_selected,
-)
-
-subgroups_group = ScrollingGroup(
-    subgroups_select,
-    id="subgroups_group",
-    width=1,
-    height=5
-)
 
 groups_select = Select(
     Format("{item.name}"),
@@ -60,12 +20,6 @@ groups_select = Select(
     items="groups",
     on_click=on_group_selected,
 )
-
-async def change_page(c, w, manager: DialogManager, delta: int):
-    page = manager.dialog_data.get("page", 0)
-    new_page = max(0, page + delta)
-    manager.dialog_data["page"] = new_page
-    await manager.show()
 
 next_button = Button(Const(text="Вперед"), id="next_button", on_click=partial(change_page, delta=+1))
 prev_button = Button(Const(text="Назад"), id="prev_button", on_click=partial(change_page, delta=-1))
