63c5cf5687d713ccff44f14e32c007ee30f2d1ef
METOKS <pressfschool@gmail.com>
2025-10-14 19:16:04 +0200

    notify

--- aiodialog/admin/admins.py
+++ aiodialog/admin/admins.py
@@ -3,7 +3,7 @@ from aiogram_dialog.widgets.text import Const, Format
 
 from aiodialog.admin.event_admin_func import admin_rename_event_selected, admin_delete_event_selected, \
     admin_edit_selected
-from aiodialog.admin.group_admin_func import admin_subgroup_selected, admin_rn_sg_selected
+from aiodialog.admin.group_admin_func import admin_subgroup_selected, admin_rn_sg_selected, admin_delete_user_selected
 
 admin_sg_select = Select(
     Format("{item.name}"),
@@ -36,7 +36,7 @@ admin_rn_sg_group = ScrollingGroup(
 )
 
 admin_delete_select = Select(
-    Format("{item[name]}"),
+    Format("{item[name]} - {item[timestamp]}"),
     id="delete_select",
     item_id_getter=lambda g: g["id"],
     items="result",
@@ -51,7 +51,7 @@ admin_delete_group = ScrollingGroup(
 )
 
 admin_rename_select = Select(
-    Format("{item[name]}"),
+    Format("{item[name]} - {item[timestamp]}"),
     id="rename_select",
     item_id_getter=lambda g: g["id"],
     items="result",
@@ -66,7 +66,7 @@ admin_rename_group = ScrollingGroup(
 )
 
 admin_time_select = Select(
-    Format("{item[name]}"),
+    Format("{item[name]} - {item[timestamp]}"),
     id="rename_select",
     item_id_getter=lambda g: g["id"],
     items="result",
@@ -81,7 +81,7 @@ admin_time_group = ScrollingGroup(
 )
 
 admin_comm_select = Select(
-    Format("{item[name]}"),
+    Format("{item[name]} - {item[timestamp]}"),
     id="comment_select",
     item_id_getter=lambda g: g["id"],
     items="result",
@@ -91,6 +91,21 @@ admin_comm_select = Select(
 admin_comm_group = ScrollingGroup(
     admin_comm_select,
     id="admin_comm_group",
+    width=1,
+    height=5
+)
+
+user_delete_select = Select(
+    Format("{item[user_id]} - {item[name]}"),
+    id="user_delete_select",
+    item_id_getter=lambda g: g["user_id"],
+    items="result",
+    on_click=admin_delete_user_selected,
+)
+
+user_delete_group = ScrollingGroup(
+    user_delete_select,
+    id="user_delete_group",
     width=1,
     height=5
 )
